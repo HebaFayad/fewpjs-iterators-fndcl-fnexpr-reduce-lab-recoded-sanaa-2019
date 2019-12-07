defb8350a31264600c4432f888c225ca46e8445e
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-07 20:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-let totalBatteries = batteryBatches.reduce(function( acc , curr){
+let total = batteryBatches.reduce(function( acc , curr){
     return acc + curr
     } , 0);
-console.log(totalBatteries)
+console.log(total)
